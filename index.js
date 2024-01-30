b241273e01ca4bd62f9e2a80fd08b9ef54b08d76
Prajwal Goutham <prajwalgouthammp@gmail.com>
2024-01-30 16:21:34 +0530

    add the ejs dependency to the initial setup

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
 const mongoose = require('mongoose')
 app.get('/', (req, res) => {
-    res.send("Hello From the Home Page")
+    res.render('home.ejs')
 })
 app.listen(3000, () => {
     console.log("Listening on port 3000")
