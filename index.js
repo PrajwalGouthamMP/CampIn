e836f0e9ae3b0e85cb0e1f83fe3e895fe30f64d2
Prajwal Goutham <prajwalgouthammp@gmail.com>
2024-01-31 15:22:44 +0530

    create the ejs file to add the new camp ground

--- index.js
+++ index.js
@@ -14,13 +14,21 @@ const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
-
+app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
     res.render('home.ejs')
 })
 app.get('/campgrounds', async (req, res) => {
     const camps = await campModel.find({})
     res.render('campgrounds/allcampg.ejs', { camps })
+})
+app.post('/campgrounds', async (req, res) => {
+    const campground = new campModel(req.body.campground)
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+app.get('/campgrounds/new', (req, res) => {
+    res.render("campgrounds/newcamp.ejs")
 })
 app.get('/campgrounds/:id', async (req, res) => {
     const { id } = req.params
