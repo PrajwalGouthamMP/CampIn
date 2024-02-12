b24919ac1c4b3e8ae8a46e081f10263a0da16a1b
Prajwal Goutham <prajwalgouthammp@gmail.com>
2024-02-12 14:26:10 +0530

    move the joi schema to a separate file

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path')
 const campModel = require('./models/campground')
 const wrapAsync = require('./utils/wrapAsync')
 const ExpressError = require('./utils/ExpressError')
+const joiSchema = require('./joischema')
 const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
 const joi = require('joi')
@@ -25,27 +26,34 @@ app.engine('ejs', ejsMate)
 app.get('/', (req, res) => {
     res.render('home.ejs')
 })
-app.get('/campgrounds', wrapAsync(async (req, res) => {
-    const camps = await campModel.find({})
-    res.render('campgrounds/allcampg.ejs', { camps })
-}))
-app.post('/campgrounds', wrapAsync(async (req, res, next) => {
-    const joiSchema = joi.object({
-        campground: joi.object({
-            title: joi.string().required(),
-            price: joi.number().required().min(0),
-            image: joi.string().required(),
-            desciption: joi.string().required(),
-            location: joi.string().required()
-        }).required()
-    })
+
+
+const validateCampground = (req, res, next) => {
+    // const joiSchema = joi.object({
+    //     campground: joi.object({
+    //         title: joi.string().required(),
+    //         price: joi.number().required().min(0),
+    //         image: joi.string().required(),
+    //         desciption: joi.string().required(),
+    //         location: joi.string().required()
+    //     }).required()
+    // })
     const { error } = joiSchema.validate(req.body)
 
 
     if (error) {
         const msg = error.details.map(el => el.message).join(',')
         throw new ExpressError(400, msg)
+    } else {
+        next()
     }
+}
+app.get('/campgrounds', wrapAsync(async (req, res) => {
+    const camps = await campModel.find({})
+    res.render('campgrounds/allcampg.ejs', { camps })
+}))
+app.post('/campgrounds', validateCampground, wrapAsync(async (req, res, next) => {
+    console.log(req.body)
     const campground = new campModel(req.body.campground)
     await campground.save()
     res.redirect(`/campgrounds/${campground._id}`)
@@ -59,7 +67,7 @@ app.get('/campgrounds/:id', wrapAsync(async (req, res) => {
     const campground = await campModel.findById(id)
     res.render('campgrounds/singlecamp.ejs', { campground })
 }))
-app.put('/campgrounds/:id', wrapAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, wrapAsync(async (req, res) => {
     const { id } = req.params
     await campModel.findByIdAndUpdate(id, req.body.campground)
     res.redirect(`/campgrounds/${id}`)
