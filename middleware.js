fc8095dcdd68d7e4252c4507777437d480d27cf6
Prajwal Goutham <prajwalgouthammp@gmail.com>
2024-02-17 16:21:39 +0530

    add the middleware to ensure that a user is logged in inorder to access our pages

---
+++ middleware.js
@@ -0,0 +1,9 @@
+module.exports.ensureLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.flash('error', 'You must be logged in first ! ')
+        return res.redirect('/login')
+    }
+    else {
+        next()
+    }
+}
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,6 +4,7 @@ const campModel = require('../models/campground')
 const wrapAsync = require('../utils/wrapAsync')
 const ExpressError = require('../utils/ExpressError')
 const { joiSchema } = require('../joischema')
+const { ensureLoggedIn } = require('../middleware')
 
 const validateCampground = (req, res, next) => {
     const { error } = joiSchema.validate(req.body)
@@ -17,21 +18,22 @@ const validateCampground = (req, res, next) => {
     }
 }
 
-route.get('/', wrapAsync(async (req, res) => {
+route.get('/', ensureLoggedIn, wrapAsync(async (req, res) => {
+
     const camps = await campModel.find({})
     res.render('campgrounds/allcampg.ejs', { camps })
 }))
-route.post('/', validateCampground, wrapAsync(async (req, res, next) => {
+route.post('/', ensureLoggedIn, validateCampground, wrapAsync(async (req, res, next) => {
     const campground = new campModel(req.body.campground)
     await campground.save()
     req.flash('success', 'Succesfully created a new Campground')
     res.redirect(`/campgrounds/${campground._id}`)
 
 }))
-route.get('/new', (req, res) => {
+route.get('/new', ensureLoggedIn, (req, res) => {
     res.render("campgrounds/newcamp.ejs")
 })
-route.get('/:id', wrapAsync(async (req, res) => {
+route.get('/:id', ensureLoggedIn, wrapAsync(async (req, res) => {
     const { id } = req.params
     const campground = await campModel.findById(id).populate('reviews')
     if (!campground) {
@@ -40,18 +42,18 @@ route.get('/:id', wrapAsync(async (req, res) => {
     }
     res.render('campgrounds/singlecamp.ejs', { campground })
 }))
-route.put('/:id', validateCampground, wrapAsync(async (req, res) => {
+route.put('/:id', ensureLoggedIn, validateCampground, wrapAsync(async (req, res) => {
     const { id } = req.params
     await campModel.findByIdAndUpdate(id, req.body.campground)
     req.flash('success', 'Succesfully updated a new Campground')
     res.redirect(`/campgrounds/${id}`)
 }))
-route.delete('/:id', wrapAsync(async (req, res) => {
+route.delete('/:id', ensureLoggedIn, wrapAsync(async (req, res) => {
     const { id } = req.params
     await campModel.findByIdAndDelete(id)
     res.redirect('/campgrounds')
 }))
-route.get('/:id/edit', wrapAsync(async (req, res) => {
+route.get('/:id/edit', ensureLoggedIn, wrapAsync(async (req, res) => {
     const { id } = req.params
     const campground = await campModel.findById(id)
     if (!campground) {
